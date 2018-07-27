62025f29a8ad9bd19abee30dc4ce078890f685ed
Mikhail Ivchenko <ematirov@gmail.com>
2018-07-27 09:58:59 +0400

    travis: test everything

--- ci/code-coverage.sh
+++ ci/code-coverage.sh
@@ -9,10 +9,10 @@ set -e
 
 echo "" > coverage.txt
 
-#for d in $(go list ./... | grep -v vendor); do
-    go test $TAGS -v -race go-hep.org/x/hep/xrootd/...
+for d in $(go list ./... | grep -v vendor); do
+    go test $TAGS -v -race $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
-#done
+done
