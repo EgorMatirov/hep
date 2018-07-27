aa02466ad054b49fa2c8a0eda91feae8a295e702
Mikhail Ivchenko <ematirov@gmail.com>
2018-07-27 09:25:56 +0400

    travis: test only xrootd

--- ci/code-coverage.sh
+++ ci/code-coverage.sh
@@ -9,10 +9,10 @@ set -e
 
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
-    go test $TAGS -race -coverprofile=profile.out -covermode=atomic $d
+#for d in $(go list ./... | grep -v vendor); do
+    go test $TAGS -race -coverprofile=profile.out -covermode=atomic go-hep.org/x/hep/xrootd/xrdfuse
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
-done
+#done
