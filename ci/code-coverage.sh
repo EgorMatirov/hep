585713751884ed5af7e3c870ed26ce01c11fad5e
Mikhail Ivchenko <ematirov@gmail.com>
2018-07-27 09:42:44 +0400

    travis: test all of xrootd

--- ci/code-coverage.sh
+++ ci/code-coverage.sh
@@ -10,7 +10,7 @@ set -e
 echo "" > coverage.txt
 
 #for d in $(go list ./... | grep -v vendor); do
-    go test $TAGS -v -race -coverprofile=profile.out -covermode=atomic -count 5 go-hep.org/x/hep/xrootd/xrdfuse
+    go test $TAGS -v -race -coverprofile=profile.out -covermode=atomic -count 5 go-hep.org/x/hep/xrootd/
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
