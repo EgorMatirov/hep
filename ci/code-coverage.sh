c36549ddc951aaaeb62738e73efdaa6de6cae00b
Mikhail Ivchenko <ematirov@gmail.com>
2018-07-27 22:36:31 +0400

    ci: typo fix

--- ci/code-coverage.sh
+++ ci/code-coverage.sh
@@ -10,7 +10,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    pushd $GOPATH/src/%d
+    pushd $GOPATH/src/$d
     go test $TAGS -race -coverprofile=profile.out -covermode=atomic -timeout 8m
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
